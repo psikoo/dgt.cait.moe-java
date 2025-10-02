4894e8cd91e187c15f7d89205aa48f975f5d1d05
psikoo <hugohernandezmuelas03@gmail.com>
2025-10-02 09:29:25 +0200

    buiild

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+echo "> dgt-java"
+git pull
+chmod +x ./build.sh
 mvn clean validate compile assembly:assembly -DdescriptorId=jar-with-dependencies
 cp ./target/autodownload-1-jar-with-dependencies.jar ./autodownload.jar
 chmod +x request/get.sh
 chmod +x request/post.sh
-docker build -t java-caitmoe:1 .
+docker build -t dgt-java:1 .
+docker compose down
+docker compose up -d
